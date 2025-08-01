c478a2d8ccf575d9304560a87557e5431b2844ff
seming23 <seming23@blackholic.com>
2025-08-01 18:14:51 +0900

    JPA 설정 및 테스트용 Member 엔티티 추가
    Spring Data JPA 설정을 추가하고 테스트용 Member 엔티티 생성 및 관련 Repository, Service, 데이터 초기화 파일 등도 함께 추가되어 테스트 데이터를 사용

--- src/main/java/me/seming/springbootdeveloper/TestController.java
+++ src/main/java/me/seming/springbootdeveloper/TestController.java
@@ -1,13 +1,18 @@
 package me.seming.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test")
-    public String test() {
-        return "Hello, world!";
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
---
+++ src/main/java/me/seming/springbootdeveloper/TestService.java
@@ -0,0 +1,14 @@
+package me.seming.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    @Autowired
+    MemberRepository memberRepository;
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+}
